0b0cf32c026e61bbf49cee8ab83a0475b676485d
SI-Pijar <pijarrizkyhentawan@gmail.com>
2024-10-05 19:13:44 +0700

    Create programKuis.cpp

---
+++ programKuis.cpp
@@ -0,0 +1,101 @@
+#include <iosteram>
+using namespace std;
+
+void iniKuis(){
+  char jawaban;
+  int nilai = 0;
+  
+  cout<<"soal 1: "<<endl;
+  cout<<"1. "<<endl;
+  cout<<"2. "<<endl;
+  cout<<"3. "<<endl;
+  cout<<"4. "<<endl;
+  cout<<"5. "<<endl;
+  cout<<"pilih jawaban (1/2/3/4/5): ";
+  cin>>jawaban;  
+    if(jawaban ==' '){
+      cout<<"Soal pertama EZ wajar bener."<<endl;
+      nilai++
+    }else{
+      cout<<"Jiahahahaha jawaban lu salah bro, yang bener....."<<endl;
+    }
+  cout<<endl;
+    
+  cout<<"soal 2: "<<endl;
+  cout<<"1. "<<endl;
+  cout<<"2. "<<endl;
+  cout<<"3. "<<endl;
+  cout<<"4. "<<endl;
+  cout<<"5. "<<endl;
+  cout<<"pilih jawaban (1/2/3/4/5): ";
+  cin>>jawaban;
+    if(jawaban ==' '){
+      cout<<"Lumayanlah."<<endl;
+      nilai++
+    }else{
+      cout<<"Masih salah? astagaa, yang bener itu....."<<endl;
+    }
+  cout<<endl;
+  
+  cout<<"soal 3: "<<endl;
+  cout<<"1. "<<endl;
+  cout<<"2. "<<endl;
+  cout<<"3. "<<endl;
+  cout<<"4. "<<endl;
+  cout<<"5. "<<endl;
+  cout<<"pilih jawaban (1/2/3/4/5): ";
+  cin>>jawaban;
+    if(jawaban ==' '){
+      cout<<"Widih! masih bener ternyata, kita naikin lah ya level soalnya."<<endl;
+      nilai++
+    }else{
+      cout<<"Bro yakin kamu sehat?, jawaban yang bener itu....."<<endl;
+    }
+  cout<<endl;
+  
+  cout<<"soal 4: "<<endl;
+  cout<<"1. "<<endl;
+  cout<<"2. "<<endl;
+  cout<<"3. "<<endl;
+  cout<<"4. "<<endl;
+  cout<<"5. "<<endl;
+  cout<<"pilih jawaban (1/2/3/4/5): ";
+  cin>>jawaban;
+    if(jawaban ==' '){
+      cout<<"Jangan seneng dulu, tim kita salah aja malah nuruin level soal, liat aja soal berikutnya."<<endl;
+      nilai++
+    }else{
+      cout<<"Dahlah bingung mau ngomong apa, jawaban yang bener itu....."<<endl;
+    }
+  cout<<endl;
+   cout<<"soal 5: "<<endl;
+  cout<<"1. "<<endl;
+  cout<<"2. "<<endl;
+  cout<<"3. "<<endl;
+  cout<<"4. "<<endl;
+  cout<<"5. "<<endl;
+  cout<<"pilih jawaban (1/2/3/4/5): ";
+  cin>>jawaban;
+    if(jawaban ==' '){
+      cout<<"keren juga lu bisa sampe sini, coba last nya 99% bukan manusia gagal"<<endl;
+      nilai++
+    }else{
+      cout<<"............., jawaban yang bener itu....."<<endl;
+    }
+  cout<<endl;
+  cout<<"soal 6: "<<endl;
+  cout<<"1. "<<endl;
+  cout<<"2. "<<endl;
+  cout<<"3. "<<endl;
+  cout<<"4. "<<endl;
+  cout<<"5. "<<endl;
+  cout<<"pilih jawaban (1/2/3/4/5): ";
+  cin>>jawaban;
+    if(jawaban ==' '){
+      cout<<"keren juga lu bisa sampe sini, coba last nya 99% yang bukan manusia gagal."<<endl;
+      nilai++
+    }else{
+      cout<<"jawaban yang bener itu....., dan karena lu salah lagi malah bikin tim kita stress, jadi udahan aja lah ya."<<endl;
+    }
+  cout<<"Nilai Anda: "<<nilai<<" dari 6."<<endl;
+ }
