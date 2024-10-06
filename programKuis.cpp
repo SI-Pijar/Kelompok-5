2423ef8884e8046a5c10fc9cf1eff3d8703b77b5
SI-Pijar <pijarrizkyhentawan@gmail.com>
2024-10-06 22:15:29 +0700

    Update programKuis12.cpp

--- programKuis.cpp
+++ programKuis.cpp
@@ -6,7 +6,7 @@ void iniKuis(){
   char jawaban;
   int nilai = 0;
   
-  cout<<"soal 1: Apa kepanjangan dari UNILA "<<endl;
+  cout<<"soal 1: Apa kepanjangan dari UNILA?"<<endl;
   cout<<"1.Universitas Lambung "<<endl;
   cout<<"2.Universitas Menyala "<<endl;
   cout<<"3.Universitas Los Angeles "<<endl;
@@ -22,12 +22,12 @@ void iniKuis(){
     }
   cout<<endl;
     
-  cout<<"soal 2: Ilmu komputer termasuk dalam fakultas ? "<<endl;
-  cout<<"1. HUKUM "<<endl;
-  cout<<"2. FEB"<<endl;
-  cout<<"3. FMIPA"<<endl;
-  cout<<"4. FKIP"<<endl;
-  cout<<"5. FT"<<endl;
+  cout<<"soal 2: Ilmu komputer termasuk dalam fakultas?"<<endl;
+  cout<<"1.HUKUM "<<endl;
+  cout<<"2.FEB"<<endl;
+  cout<<"3.FMIPA"<<endl;
+  cout<<"4 FKIP"<<endl;
+  cout<<"5.FT"<<endl;
   cout<<"pilih jawaban (1/2/3/4/5): ";
   cin>>jawaban;
     if(jawaban =='3'){
@@ -39,11 +39,11 @@ void iniKuis(){
   cout<<endl;
   
   cout<<"soal 3: Dibawah ini yang termasuk program studi di jurusan Ilmu komputer adalah "<<endl;
-  cout<<"1. Matematika "<<endl;
-  cout<<"2. Farmasi "<<endl;
-  cout<<"3. pendidikan teknologi informasi "<<endl;
-  cout<<"4. Sistem Informasi"<<endl;
-  cout<<"5. Peternakan "<<endl;
+  cout<<"1.Matematika "<<endl;
+  cout<<"2.Farmasi "<<endl;
+  cout<<"3.pendidikan teknologi informasi "<<endl;
+  cout<<"4.Sistem Informasi"<<endl;
+  cout<<"5.Peternakan "<<endl;
   cout<<"pilih jawaban (1/2/3/4/5): ";
   cin>>jawaban;
     if(jawaban =='4'){
@@ -54,7 +54,7 @@ void iniKuis(){
     }
   cout<<endl;
   
-  cout<<"soal 4: Siapa dosen kesukaan Mahasiswa Jurusan ILMU KOMPUTER ? "<<endl;
+  cout<<"soal 4: Siapa dosen kesukaan Mahasiswa Jurusan ILMU KOMPUTER? "<<endl;
   cout<<"1.Bapak Yhoga "<<endl;
   cout<<"2.Bapak Ridho "<<endl;
   cout<<"3.Bapak Dwi Sakheti:) "<<endl;
@@ -69,7 +69,8 @@ void iniKuis(){
       cout<<"Apasih, gitu aja salah, Yang bener itu Bapak Dwi Sakheti."<<endl;
     }
   cout<<endl;
-   cout<<"soal 5: Siapa KAPRODI Sistem Informasi "<<endl;
+  
+  cout<<"soal 5: Siapa KAPRODI Sistem Informasi?"<<endl;
   cout<<"1.Bapak Dwi Shaketi "<<endl;
   cout<<"2.Bapak Mulyono "<<endl;
   cout<<"3.Umi eka "<<endl;
@@ -86,11 +87,11 @@ void iniKuis(){
   cout<<endl;
   
   cout<<"soal 6: Apa yang membuat mahasiswa ilmu komputer begadang? "<<endl;
-  cout<<"1. nongki "<<endl;
-  cout<<"2. ngoding "<<endl;
-  cout<<"3. mabar ml"<<endl;
-  cout<<"4. nonton"<<endl;
-  cout<<"5. ngedate"<<endl;
+  cout<<"1.nongki "<<endl;
+  cout<<"2.ngoding "<<endl;
+  cout<<"3.mabar ml"<<endl;
+  cout<<"4.nonton"<<endl;
+  cout<<"5.ngedate"<<endl;
   cout<<"pilih jawaban (1/2/3/4/5): ";
   cin>>jawaban;
     if(jawaban =='2'){
@@ -101,7 +102,7 @@ void iniKuis(){
     }
     cout<<endl;
   
-  cout<<"soal 7:Jika mahasiswa Ilmu Komputer ditanya tentang masa depan, mereka akan menjawab: "<<endl;
+  cout<<"soal 7: Jika mahasiswa Ilmu Komputer ditanya tentang masa depan, mereka akan menjawab! "<<endl;
   cout<<"1.Pasti cerah "<<endl;
   cout<<"2.Tergantung Koneksi internet "<<endl;
   cout<<"3.Akan penuh dengan bug "<<endl;
@@ -117,9 +118,9 @@ void iniKuis(){
     }
   cout<<endl;
   
-  cout<<"soal 8:Jika ditanya tentang cinta, mahasiswa Ilmu Komputer akan menjawab "<<endl;
+  cout<<"soal 8: Jika ditanya tentang cinta, mahasiswa Ilmu Komputer akan menjawab "<<endl;
   cout<<"1.Error 404: Cinta Tidak Ditemukan "<<endl;
-  cout<<"2.Saya hanya mencari ‘compatible’ "<<endl;
+  cout<<"2.Bodo amat GAK PENTING!!!!! "<<endl;
   cout<<"3.cout <<'ya tuhan,Satukan aku dengan dia'<<end1; "<<endl;
   cout<<"4.Cinta itu seperti variabel, harus diinisialisasi! "<<endl;
   cout<<"5.Cinta itu terlalu kompleks, mending coding "<<endl;
@@ -133,11 +134,11 @@ void iniKuis(){
     }
   cout<<endl;
   cout<<"soal 9: Apa yang kamu harapkan setelah belajar Ilmu Komputer? "<<endl;
-  cout<<"1. Mampu berbicara dengan komputer dalam bahasa rahasia."<<endl;
-  cout<<"2. Mengembangkan aplikasi untuk mengingatkan orang tentang deadline.."<<endl;
-  cout<<"3. Membuat AI yang bisa menggantikan dosen. "<<endl;
-  cout<<"4. Supaya bisa bilang Saya programmer dengan percaya diri."<<endl;
-  cout<<"5.  Menyusun rencana untuk menguasai dunia melalui kode "<<endl;
+  cout<<"1.Mampu berbicara dengan komputer dalam bahasa rahasia."<<endl;
+  cout<<"2.Mengembangkan aplikasi untuk mengingatkan orang tentang deadline.."<<endl;
+  cout<<"3.Membuat AI yang bisa menggantikan dosen. "<<endl;
+  cout<<"4.Supaya bisa bilang Saya programmer dengan percaya diri."<<endl;
+  cout<<"5.Menyusun rencana untuk menguasai dunia melalui kode "<<endl;
   cout<<"pilih jawaban (1/2/3/4/5): ";
   cin>>jawaban;
     if(jawaban =='5'){
@@ -162,7 +163,7 @@ void iniKuis(){
     }else{
       cout<<"Sebenernya Jawabanmu Bener,tapi Salah."<<endl;
     } 
-  cout << "Skor Anda " << nilai << "dari 10." << endl;
+  cout << "Skor Anda " << nilai << " dari 10." << endl;
 }
 int main (){
   int pilihan;
